31dd9f4515f237337784033e862e231ca1c00665
laurent <laurent.bodin58@gmail.com>
2020-03-11 14:07:17 +0100

    About and home activity

---
+++ app/src/main/java/com/tania/cinequizz/AboutActivity.java
@@ -0,0 +1,31 @@
+package com.tania.cinequizz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        goToHome();
+    }
+
+    private void goToHome() {
+        final Intent intent = new Intent(this,HomeActivity.class);
+
+        ImageView homeButton = findViewById(R.id.homeButton);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+    }
+}

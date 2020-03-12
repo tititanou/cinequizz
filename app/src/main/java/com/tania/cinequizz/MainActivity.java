b026c8b382eb3689b48fd6d849ff04e390f6c33f
Unknown <xxtoinou92xx@gmail.com>
2020-03-12 13:03:07 +0100

    liste question

--- app/src/main/java/com/tania/cinequizz/MainActivity.java
+++ app/src/main/java/com/tania/cinequizz/MainActivity.java
@@ -29,14 +29,33 @@ public class MainActivity extends AppCompatActivity {
 
     private int state = STATE_INIT;
 
-    Answer answer = new Answer(R.drawable.poudlard, "image", "De quel film est tirée cette image ?", "Harry Potter", "robinonekenoby", "tania");
+    Answer answer1= new Answer(R.drawable.poudlard, "image", "De quel film est tirée cette image ?", "Harry Potter", "robinonekenoby", "tania");
     Answer answer2 = new Answer(R.raw.polish_oss, "audio", "De quel film est tiré cet extrait audio?", "OSS 117", "Le père noël est une ordure", "Need for speed");
+    Answer answer3 = new Answer(R.drawable.avatar, "image","De quel film est tirée cette image ?","Avatar","Alien","Mars Attack");
+    Answer answer4 = new Answer(R.drawable.belle_bete_cocteau,"image","De quel film est tirée cette image ?","La belle et la bête","Peau d'Âne", "Le Magicien D'Oz");
+    Answer answer5 = new Answer(R.drawable.bridget_jones,"image","De quel film est tirée cette image ?","Le Journal de Bridget Jones", "Bye Bye Love","Retour a Cold Mountain";
+    Answer answer6 = new Answer(R.drawable.cage_aux_folles, "image","De quel film est tirée cette image ?","La Cage aux Folles","La Cage Dorée","Le Placard")
+    Answer answer7 = new Answer(R.drawable.edward_mains_argent,"image","De quel film est tirée cette image ?","Edward aux Mains D'argent", "La Reine des Neiges","Dark Shadows");
+    Answer answer8 = new Answer(R.drawable.etrange_noel_mr_jack,"image","De quel film est tirée cette image ?","L'Etrange Noël de Mr.Jack","Les Noces Funèbres","Frankenweenie");
+    Answer answer9 = new Answer(R.drawable.fifty_shades_grey,"image","De quel film est tirée cette image ?","50 Nuances de Grey","After","Vous Avez un Message");
+    Answer answer10 = new Answer(R.drawable.grande_vadrouille,"image","De quel film est tirée cette image ?","La Grande Vadrouille","La 7eme Compagnie","Les Gendarmes à St Tropez");
+    Answer answer11 = new Answer(R.drawable.kingkong,"image","De quel film est tirée cette image ?","KingKong","La Planète des Singes","Godzilla");
+    Answer answer12 = new Answer(R.drawable.pretty_woman,"image","De quel film est tirée cette image ?","Pretty Woman","Le Sourire de Mona Lisa","Erine Brockovich");
+    Answer answer13 = new Answer(R.drawable.mala_educacion,"image","De quel film est tirée cette image ?","La Mauvaise Education","Camping","Coup de Tête");
+    Answer answer14 = new Answer(R.drawable.lauwrence_arabie,"image","De quel film est tirée cette image ?","Lauwrence D'Arabie","Ben Hûr", "Gandhi");
+    Answer answer15 = new Answer(R.drawable.slumdog_millionnaire,"image","De quel film est tirée cette image ?","SlumDog Millionaire","Indian Palace","Salaam Bombay");
+    Answer answer16 = new Answer(R.drawable.titanic,"image","De quel film est tirée cette image ?","Titanic","Poséidon","La Forme de l'Eau");
+    Answer answer17 = new Answer(R.drawable.seigneur_des_anneaux,"image","De quel film est tirée cette image ?","Le Seigneur des Anneaux","Le Hobbit","Harry Potter");
 
 
 // met en place une liste de questions
 
 // un compteur
 // une fonction qui incrémente ce compteur et chnage le texte du bouton
+
+    public List<String> questions(){
+        
+    }
 
 
     @Override
@@ -61,6 +80,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        ArrayList<Answer> quizz = new ArrayList<>();
+        quizz.add(answer1);
+        quizz.add(answer2);
+        quizz.add(answer3);
+        quizz.add(answer4);
+        quizz.add(answer5);
+        quizz.add(answer6);
+        quizz.add(answer7);
+        quizz.add(answer8);
+        quizz.add(answer9);
+        quizz.add(answer10);
+        quizz.add(answer11);
+        quizz.add(answer12);
+        quizz.add(answer13);
+        quizz.add(answer14);
+        quizz.add(answer15);
+        quizz.add(answer16);
+        quizz.add(answer17);
+
         ImageView filmImageView = findViewById(R.id.filmImageView);
         filmImageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,8 +109,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // creation des questions/reponses
-        question.setText(answer.getQuestion());
-        List<String> answered = init(answer);
+        question.setText(answer1.getQuestion());
+        List<String> answered = init(answer1);
         final RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
         for (int i = 0; i < radioGroup.getChildCount(); i++) {
             ((RadioButton) radioGroup.getChildAt(i)).setText(String.valueOf(answered.get(i)));
@@ -97,6 +135,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
     }
 
 
@@ -105,7 +144,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private void imageZoom() {
         final Intent intent = new Intent(this, ZoomActivity.class);
-        intent.putExtra("image", answer);
+        intent.putExtra("image", answer1);
         startActivity(intent);
     }
 
@@ -113,7 +152,7 @@ public class MainActivity extends AppCompatActivity {
      * if audio media shows only button play else shows only image media
      */
     private void mediaTest() {
-        if (answer.getMediaType().equals("audio")) {
+        if (answer1.getMediaType().equals("audio")) {
             findViewById(R.id.filmImageView).setVisibility(View.INVISIBLE);
             findViewById(R.id.buttonPlayer).setVisibility(View.VISIBLE);
         } else {
@@ -139,10 +178,10 @@ public class MainActivity extends AppCompatActivity {
      */
 
     private void checkAnswer(CharSequence answered) {
-        if (answered.equals(answer.getRightAnswer())) {
+        if (answered.equals(answer1.getRightAnswer())) {
             msgTextView.setText("Bonne réponse !");
         } else {
-            msgTextView.setText("La bonne réponse était \"" + answer.getRightAnswer() + "\".");
+            msgTextView.setText("La bonne réponse était \"" + answer1.getRightAnswer() + "\".");
         }
     }
 

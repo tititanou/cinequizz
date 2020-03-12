0d8eaad1f86ad4b3b2d54352f5d0851437f0fcc9
cassel boris <casselboris92@gmail.com>
2020-03-12 14:41:24 +0100

    refcator du code et random questions et fonction init

--- app/src/main/java/com/tania/cinequizz/MainActivity.java
+++ app/src/main/java/com/tania/cinequizz/MainActivity.java
@@ -13,7 +13,6 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -26,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView msgTextView;
     private TextView question;
     private boolean nextQuestion = true;
+    private int index;
+    private int score;
 
     private int state = STATE_INIT;
 
@@ -33,8 +34,8 @@ public class MainActivity extends AppCompatActivity {
     Answer answer2 = new Answer(R.raw.polish_oss, "audio", "De quel film est tiré cet extrait audio?", "OSS 117", "Le père noël est une ordure", "Need for speed");
     Answer answer3 = new Answer(R.drawable.avatar, "image","De quel film est tirée cette image ?","Avatar","Alien","Mars Attack");
     Answer answer4 = new Answer(R.drawable.belle_bete_cocteau,"image","De quel film est tirée cette image ?","La belle et la bête","Peau d'Âne", "Le Magicien D'Oz");
-    Answer answer5 = new Answer(R.drawable.bridget_jones,"image","De quel film est tirée cette image ?","Le Journal de Bridget Jones", "Bye Bye Love","Retour a Cold Mountain";
-    Answer answer6 = new Answer(R.drawable.cage_aux_folles, "image","De quel film est tirée cette image ?","La Cage aux Folles","La Cage Dorée","Le Placard")
+    Answer answer5 = new Answer(R.drawable.bridget_jones,"image","De quel film est tirée cette image ?","Le Journal de Bridget Jones", "Bye Bye Love","Retour a Cold Mountain");
+    Answer answer6 = new Answer(R.drawable.cage_aux_folles, "image","De quel film est tirée cette image ?","La Cage aux Folles","La Cage Dorée","Le Placard");
     Answer answer7 = new Answer(R.drawable.edward_mains_argent,"image","De quel film est tirée cette image ?","Edward aux Mains D'argent", "La Reine des Neiges","Dark Shadows");
     Answer answer8 = new Answer(R.drawable.etrange_noel_mr_jack,"image","De quel film est tirée cette image ?","L'Etrange Noël de Mr.Jack","Les Noces Funèbres","Frankenweenie");
     Answer answer9 = new Answer(R.drawable.fifty_shades_grey,"image","De quel film est tirée cette image ?","50 Nuances de Grey","After","Vous Avez un Message");
@@ -47,15 +48,10 @@ public class MainActivity extends AppCompatActivity {
     Answer answer16 = new Answer(R.drawable.titanic,"image","De quel film est tirée cette image ?","Titanic","Poséidon","La Forme de l'Eau");
     Answer answer17 = new Answer(R.drawable.seigneur_des_anneaux,"image","De quel film est tirée cette image ?","Le Seigneur des Anneaux","Le Hobbit","Harry Potter");
 
+    Answer answer18= new Answer(R.raw.cite_peur, "audio", "De quel film est tiré ce son ?", "La cité de la peur", "Camping", "Sans peur et sans reproche");
+    Answer answer19= new Answer(R.raw.lebowski, "audio","De quel film est tiré ce son ?", "The Big Lebowski", "Rain man", "Narnia");
 
-// met en place une liste de questions
 
-// un compteur
-// une fonction qui incrémente ce compteur et chnage le texte du bouton
-
-    public List<String> questions(){
-        
-    }
 
 
     @Override
@@ -65,21 +61,10 @@ public class MainActivity extends AppCompatActivity {
 
 
         setTitle("CineQuizz");
-        msgTextView = findViewById(R.id.msgTextView);
-        validateButton = findViewById(R.id.validateButton);
-        question = findViewById(R.id.questionTextView);
 
-        mediaTest();
-
-        Button buttonPlayer = findViewById(R.id.buttonPlayer);
-        buttonPlayer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                playMedia();
-
-            }
-        });
-
+        /**
+         * creation of our questions list
+         */
         ArrayList<Answer> quizz = new ArrayList<>();
         quizz.add(answer1);
         quizz.add(answer2);
@@ -98,25 +83,69 @@ public class MainActivity extends AppCompatActivity {
         quizz.add(answer15);
         quizz.add(answer16);
         quizz.add(answer17);
+        quizz.add(answer18);
+        quizz.add(answer19);
 
-        ImageView filmImageView = findViewById(R.id.filmImageView);
-        filmImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                imageZoom();
 
-            }
-        });
+        /**
+         * initilization of the index and the score, shuffling of our questions list
+         */
+        initialization(quizz);
 
-        // creation des questions/reponses
-        question.setText(answer1.getQuestion());
-        List<String> answered = init(answer1);
-        final RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
-        for (int i = 0; i < radioGroup.getChildCount(); i++) {
-            ((RadioButton) radioGroup.getChildAt(i)).setText(String.valueOf(answered.get(i)));
+
+         msgTextView = findViewById(R.id.msgTextView);
+         validateButton = findViewById(R.id.validateButton);
+         question = findViewById(R.id.questionTextView);
+
+        /**
+         *  we choose an object (an answer) in our list
+        */
+        final Answer answerChoosed = quizz.get(index);
+
+
+        /**
+         * we set the question to text view
+         */
+        question.setText(answerChoosed.getQuestion());
+
+
+        /**
+         * check the type of our data and set the media to the media view
+         */
+        if (mediaTest(answerChoosed).equals("audio")){
+                Button buttonPlayer = findViewById(R.id.buttonPlayer);
+                buttonPlayer.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        playMedia(answerChoosed);
+
+                    }
+                });
+
+        }
+        else if (mediaTest(answerChoosed).equals("image")){
+            ImageView filmImageView = findViewById(R.id.filmImageView);
+            filmImageView.setImageResource(answerChoosed.getMedia());
+            filmImageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    imageZoom(answerChoosed);
+                }
+            });
+
         }
 
-        Log.i("MainActivity", "onCreate: ");
+        /**
+         * 1) we shuffle the order of our questions
+         * 2) we set the answers to the view
+         */
+        List<String> answerToDisplay = shuffleAnswers(answerChoosed);
+        final RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        for (int i = 0; i < radioGroup.getChildCount(); i++) {
+            ((RadioButton) radioGroup.getChildAt(i)).setText(String.valueOf(answerToDisplay.get(i)));
+        }
+
+
 
         validateButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -125,7 +154,7 @@ public class MainActivity extends AppCompatActivity {
                     int id = radioGroup.getCheckedRadioButtonId();
                     RadioButton radioButton = findViewById(id);
                     CharSequence radioAnswer = radioButton.getText();
-                    checkAnswer(radioAnswer);
+                    checkAnswer(radioAnswer, answerChoosed);
                     validateButton.setText("Question suivante");
                     state = STATE_VALIDATED;
                 }
@@ -142,33 +171,16 @@ public class MainActivity extends AppCompatActivity {
     /**
      * grows image in other activity
      */
-    private void imageZoom() {
+    private void imageZoom(Answer answer) {
         final Intent intent = new Intent(this, ZoomActivity.class);
-        intent.putExtra("image", answer1);
+        intent.putExtra("image", answer);
         startActivity(intent);
     }
 
-    /**
-     * if audio media shows only button play else shows only image media
-     */
-    private void mediaTest() {
-        if (answer1.getMediaType().equals("audio")) {
-            findViewById(R.id.filmImageView).setVisibility(View.INVISIBLE);
-            findViewById(R.id.buttonPlayer).setVisibility(View.VISIBLE);
-        } else {
-            findViewById(R.id.filmImageView).setVisibility(View.VISIBLE);
-            findViewById(R.id.buttonPlayer).setVisibility(View.INVISIBLE);
-        }
 
-    }
 
-    /**
-     * This function play the media in raw
-     */
-    private void playMedia() {
-        MediaPlayer mp = MediaPlayer.create(this, R.raw.polish_oss);
-        mp.start();
-    }
+
+
 
     /**
      * This function checks the answer.
@@ -177,11 +189,11 @@ public class MainActivity extends AppCompatActivity {
      *                 return nothing. but display a message and swap the button 'validate' to 'question suivante'.
      */
 
-    private void checkAnswer(CharSequence answered) {
-        if (answered.equals(answer1.getRightAnswer())) {
+    private void checkAnswer(CharSequence answered, Answer solution) {
+        if (answered.equals(solution.getRightAnswer())) {
             msgTextView.setText("Bonne réponse !");
         } else {
-            msgTextView.setText("La bonne réponse était \"" + answer1.getRightAnswer() + "\".");
+            msgTextView.setText("La bonne réponse était \"" + solution.getRightAnswer() + "\".");
         }
     }
 
@@ -196,7 +208,7 @@ public class MainActivity extends AppCompatActivity {
      *
      * @return a list of answers
      */
-    public List<String> init(Answer answer) {
+    public List<String> shuffleAnswers(Answer answer) {
         // on met les réponses dans un tab
         ArrayList<String> answered = new ArrayList<String>();
         answered.add(answer.getRightAnswer());
@@ -206,5 +218,46 @@ public class MainActivity extends AppCompatActivity {
         return answered;
     }
 
+    public List<Answer> shuffleList(List<Answer> listing){
+        Collections.shuffle(listing);
+        return listing;
+
+    }
+
+
+    public void initialization (List<Answer> myList){
+        List<Answer> myListShuffled = shuffleList(myList);
+        index = 0;
+        state = STATE_INIT;
+        score =0;
+    }
+
+
+
+    /**
+     * if audio media shows only button play else shows only image media
+     */
+    private String mediaTest(Answer answer) {
+        if (answer.getMediaType().equals("audio")) {
+            findViewById(R.id.filmImageView).setVisibility(View.INVISIBLE);
+            findViewById(R.id.buttonPlayer).setVisibility(View.VISIBLE);
+            return "audio";
+
+        } else {
+            findViewById(R.id.filmImageView).setVisibility(View.VISIBLE);
+            findViewById(R.id.buttonPlayer).setVisibility(View.INVISIBLE);
+            return "image";
+        }
+    }
+
+
+
+    /**
+     * This function play the media in raw
+     */
+    private void playMedia(Answer answer) {
+        MediaPlayer mp = MediaPlayer.create(this, answer.getMedia());
+        mp.start();
+    }
 
 }
